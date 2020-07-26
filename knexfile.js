d5b076abf285469b5ac856814b9a18133888610e
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-07-26 15:38:26 -0700

    fixed the sqlite issue

--- knexfile.js
+++ knexfile.js
@@ -46,7 +46,9 @@ module.exports = {
       max: 10
     },
     migrations: {
-        directory: "./database/migrations"
+        directory: "./database/migrations",
+        tableName: "knex_migrations"
+    
       }
   }
 
